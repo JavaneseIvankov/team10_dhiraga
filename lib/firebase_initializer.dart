8dfd7245f6164592a7b45a10736374f613322746
arundaya <runsworkspace@gmail.com>
2025-03-06 12:37:06 +0700

    chore: clean up unused file

--- lib/firebase_initializer.dart
+++
@@ -1,9 +0,0 @@
-import 'package:firebase_core/firebase_core.dart';
-
-import 'core/services/firebase_options.dart';
-
-// Contoh inisialisasi firebase, nnti dihapus aja
-
-void main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-}
