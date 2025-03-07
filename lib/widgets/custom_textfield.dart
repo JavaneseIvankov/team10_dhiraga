0d9eaa078670bd9ed3c4cb7197a378fac034c317
arundaya <runsworkspace@gmail.com>
2025-03-08 01:12:52 +0700

    Menambahkan text style ke parameter widget large text, menambahkan text sebagai label pada textfield

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -4,38 +4,51 @@ import 'package:team10_dhiraga/core/theme/app_color.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final String placeholder;
   final bool isPassword;
 
   CustomTextField({
     required this.controller,
     required this.label,
+    required this.placeholder,
     this.isPassword = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      obscureText: isPassword,
-      decoration: InputDecoration(
-        labelText: label,
-        floatingLabelBehavior: FloatingLabelBehavior.never,
-        labelStyle: const TextStyle(color: AppColors.grey),
-        filled: true,
-        fillColor: AppColors.background,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide(color: AppColors.grey, width: 2),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          textAlign: TextAlign.left,
+          // style: TextStyle(fontWeight: FontWeight.w400),
         ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide(color: Colors.white, width: 10),
+        SizedBox(height: 5),
+        TextField(
+          controller: controller,
+          obscureText: isPassword,
+          decoration: InputDecoration(
+            labelText: placeholder,
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            labelStyle: const TextStyle(color: AppColors.grey),
+            filled: true,
+            fillColor: AppColors.background,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: AppColors.grey, width: 2),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(color: Colors.white, width: 10),
+            ),
+            contentPadding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 20,
+            ),
+          ),
         ),
-        contentPadding: const EdgeInsets.symmetric(
-          vertical: 16,
-          horizontal: 20,
-        ),
-      ),
+      ],
     );
   }
 }
--- lib/widgets/large_text.dart
+++ lib/widgets/large_text.dart
@@ -4,7 +4,14 @@ import 'package:team10_dhiraga/core/theme/app_color.dart';
 class LargeText extends StatelessWidget {
   final String text;
   final FontStyle? fontStyle;
-  const LargeText({super.key, required this.text, this.fontStyle});
+  final TextStyle? textStyle;
+
+  const LargeText({
+    super.key,
+    required this.text,
+    this.fontStyle,
+    this.textStyle,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +21,21 @@ class LargeText extends StatelessWidget {
         fontSize: 32,
         fontWeight: FontWeight.w700,
         color: AppColors.primary ?? Colors.black,
-      ).copyWith(fontStyle: fontStyle),
+      ).copyWith(
+        fontStyle: fontStyle,
+        fontSize: textStyle?.fontSize,
+        fontWeight: textStyle?.fontWeight,
+        color: textStyle?.color,
+        letterSpacing: textStyle?.letterSpacing,
+        wordSpacing: textStyle?.wordSpacing,
+        height: textStyle?.height,
+        backgroundColor: textStyle?.backgroundColor,
+        decoration: textStyle?.decoration,
+        decorationColor: textStyle?.decorationColor,
+        decorationStyle: textStyle?.decorationStyle,
+        decorationThickness: textStyle?.decorationThickness,
+        shadows: textStyle?.shadows,
+      ),
     );
   }
 }
