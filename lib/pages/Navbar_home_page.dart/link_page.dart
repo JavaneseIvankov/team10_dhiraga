1ccc58c03b0ef82af467b33cf15f52267e643e60
lipkusuma <alifkusum4@student.ub.ac.id>
2025-03-20 06:40:54 +0700

    integrasi sebuah link untuk mengalihkannya ke sebuah website

--- lib/pages/Navbar_home_page.dart/link_page.dart
+++ lib/pages/Navbar_home_page.dart/link_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LinkPage extends StatelessWidget {
   final String link; // Parameter untuk menyimpan link yang diklik
