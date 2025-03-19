7849fe00214ff07d6c317dce45d87d24b2c363be
arundaya <runsworkspace@gmail.com>
2025-03-19 21:51:44 +0700

    menambahkan opsi untuk menyembunyikan password pada custom textfield

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:team10_dhiraga/core/theme/app_color.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final String placeholder;
@@ -16,21 +16,40 @@ class CustomTextField extends StatelessWidget {
   });
 
   @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.isPassword;
+  }
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          label,
+          widget.label,
           textAlign: TextAlign.left,
           // style: TextStyle(fontWeight: FontWeight.w400),
         ),
         SizedBox(height: 5),
         TextField(
-          controller: controller,
-          obscureText: isPassword,
+          controller: widget.controller,
+          obscureText: _obscureText,
           decoration: InputDecoration(
-            labelText: placeholder,
+            labelText: widget.placeholder,
             floatingLabelBehavior: FloatingLabelBehavior.never,
             labelStyle: const TextStyle(color: AppColors.grey),
             filled: true,
@@ -47,6 +66,16 @@ class CustomTextField extends StatelessWidget {
               vertical: 16,
               horizontal: 20,
             ),
+            suffixIcon:
+                widget.isPassword
+                    ? IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility : Icons.visibility_off,
+                        color: AppColors.grey,
+                      ),
+                      onPressed: _togglePasswordVisibility,
+                    )
+                    : null,
           ),
         ),
       ],
