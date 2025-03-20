fdc71f8173e1d71cd2126568c4326b6f5bb2a717
lipkusuma <alifkusum4@student.ub.ac.id>
2025-03-21 02:30:35 +0700

    mengupadate kode agar bisa singkron dengan notification_provider

--- lib/pages/Navbar_event_page.dart/notification_page.dart
+++ lib/pages/Navbar_event_page.dart/notification_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'notification_provider.dart';
 
 class NotificationPage extends StatelessWidget {
-  const NotificationPage({super.key});
-
   @override
   Widget build(BuildContext context) {
+    final notificationProvider = Provider.of<NotificationProvider>(context);
+    final savedEvents = notificationProvider.savedNotifications;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -18,44 +21,33 @@ class NotificationPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
       ),
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromRGBO(218, 216, 219, 1), // Warna gradasi atas
-              Color.fromARGB(255, 203, 176, 207), // Warna gradasi bawah
-            ],
-          ),
-        ),
-        child: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Belum ada beasiswa dan webinar yang disimpan.",
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-              SizedBox(height: 8),
-              Text(
-                "Kembali ke Event!",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 44, 74, 221),
+        padding: EdgeInsets.all(16),
+        child:
+            savedEvents.isEmpty
+                ? Center(
+                  child: Text("Belum ada beasiswa dan webinar yang disimpan."),
+                )
+                : ListView.builder(
+                  itemCount: savedEvents.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(savedEvents[index]['title']!),
+                        subtitle: Text(savedEvents[index]['date']!),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            notificationProvider.removeNotification(
+                              savedEvents[index]['title']!,
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                  },
                 ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
