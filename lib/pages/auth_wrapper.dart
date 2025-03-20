0956e74bbe0fb612d101e666c983ff77c69c6050
lipkusuma <alifkusum4@student.ub.ac.id>
2025-03-21 02:28:16 +0700

    mengupdate kode supaya bisa menyinkronkan dengan notifikasi

--- lib/pages/auth_wrapper.dart
+++ lib/pages/auth_wrapper.dart
@@ -4,6 +4,19 @@ import 'package:team10_dhiraga/pages/Navbar_home_page.dart/home_page.dart';
 import 'package:team10_dhiraga/pages/login_page.dart';
 
 class AuthWrapper extends StatelessWidget {
+  final List<Map<String, String>> savedNotifications;
+  final Function(Map<String, String>) addNotification;
+
+  const AuthWrapper({
+    Key? key,
+    this.savedNotifications = const [],
+    this.addNotification = _defaultAddNotification, // Nilai default
+  }) : super(key: key);
+
+  static void _defaultAddNotification(Map<String, String> notification) {
+    // Fungsi default jika tidak diberikan
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
@@ -13,7 +26,10 @@ class AuthWrapper extends StatelessWidget {
           return Scaffold(body: Center(child: CircularProgressIndicator()));
         }
         if (snapshot.hasData) {
-          return HomePage();
+          return HomePage(
+            savedNotifications: savedNotifications,
+            addNotification: addNotification,
+          );
         }
         return LoginPage();
       },
